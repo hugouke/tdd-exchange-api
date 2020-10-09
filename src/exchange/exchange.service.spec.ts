34fca546ea9b0a0ee4cf5088993671fec37ddbce
Hugo Nogueira <nogueiradeoliveira@gmail.com>
2020-10-09 02:37:24 -0300

    ensure convertAmount called getCurrent twice

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,16 +1,24 @@
 import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ExchangeService } from './exchange.service';
+import { CurrenciesService, ExchangeService } from './exchange.service';
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
+  let currenciesService: CurrenciesService;
 
   beforeEach(async () => {
+    const currenciesServiceMock = {
+      getCurrency: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
+      providers: [
+        ExchangeService,
+        { provide: CurrenciesService, useFactory: () => currenciesServiceMock },
+      ],
     }).compile();
 
     service = module.get<ExchangeService>(ExchangeService);
+    currenciesService = module.get<CurrenciesService>(CurrenciesService);
   });
 
   it('should be defined', () => {
@@ -28,6 +36,11 @@ describe('ExchangeService', () => {
       await expect(
         service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 }),
       ).resolves.not.toThrow();
+    });
+
+    it('should be called getCurrency twice', async () => {
+      await service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 });
+      await expect(currenciesService.getCurrency).toBeCalledTimes(2);
     });
   });
 });
--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,10 +1,19 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 
+export class CurrenciesService {
+  async getCurrency(currency: string): Promise<any> {}
+}
+
 @Injectable()
 export class ExchangeService {
+  constructor(private currenciesService: CurrenciesService) {}
+
   async convertAmount({ from, to, amount }): Promise<any> {
     if (!from || !to || !amount) {
       throw new BadRequestException();
     }
+
+    const currencyFrom = this.currenciesService.getCurrency(from);
+    const currencyTo = this.currenciesService.getCurrency(to);
   }
 }
