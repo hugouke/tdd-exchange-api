412abc8a6473cbe94b85880e33b7810b2e79a2bb
Hugo Nogueira <nogueiradeoliveira@gmail.com>
2020-10-12 11:06:32 -0300

    updating currencies entity

--- src/currencies/currencies.entity.ts
+++ src/currencies/currencies.entity.ts
@@ -1,4 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ObjectIdColumn,
+  PrimaryColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Unique(['currency'])
+@Entity()
 export class Currencies {
+  @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryColumn()
   currency: string;
+
+  @Column()
   value: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updateAt: Date;
 }
